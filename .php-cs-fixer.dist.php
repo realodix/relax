de023d0d230ba723dba1ef1561821aa96a9c104c
Kei <budi03@gmail.com>
2024-03-17 17:28:49 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,4 +11,5 @@ $finder = Finder::base()
     ->append(['.php-cs-fixer.dist.php', 'bin/relax']);
 
 return Config::create('@Realodix', $localRules)
-    ->setFinder($finder);
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/.tmp/.php-cs-fixer.cache');
