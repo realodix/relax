4ec17f268bbec7144898e8101d22732ace53233f
Kei <budi03@gmail.com>
2024-07-09 07:52:36 +0700

    Update realodix ruleset

--- src/RuleSet/Sets/RealodixPlus.php
+++ src/RuleSet/Sets/RealodixPlus.php
@@ -2,45 +2,20 @@
 
 namespace Realodix\Relax\RuleSet\Sets;
 
-use PhpCsFixerCustomFixers\Fixer;
 use Realodix\Relax\RuleSet\AbstractRuleSet;
 
+/**
+ * @deprecated use `RelaxPlus` instead
+ */
 final class RealodixPlus extends AbstractRuleSet
 {
     /**
      * Inherit the rules from Realodix
      *
-     * @see \Realodix\Relax\RuleSet\Sets\Realodix
+     * @see \Realodix\Relax\RuleSet\Sets\RelaxPlus
      */
     public function rules(): array
     {
-        return array_merge((new Realodix)->rules(), $this->mainRules());
-    }
-
-    /**
-     * Returns the main rules
-     *
-     * @internal
-     */
-    public function mainRules(): array
-    {
-        return [
-            'explicit_string_variable' => true,
-            'no_superfluous_elseif' => true,
-            'general_phpdoc_annotation_remove' => [
-                'annotations' => [
-                    // https://github.com/doctrine/coding-standard/blob/3e88327/lib/Doctrine/ruleset.xml#L227
-                    'api', 'author', 'category', 'copyright', 'created', 'license', 'package', 'since',
-                    'subpackage', 'version',
-                    // https://github.com/laminas/laminas-coding-standard/blob/9825280/src/LaminasCodingStandard/ruleset.xml#L883
-                    'expectedException', 'expectedExceptionCode', 'expectedExceptionMessage', 'expectedExceptionMessageRegExp',
-                ],
-            ],
-            'php_unit_method_casing' => true,
-
-            Fixer\CommentSurroundedBySpacesFixer::name() => true,
-            Fixer\NoDoctrineMigrationsGeneratedCommentFixer::name() => true,
-            Fixer\NoUselessCommentFixer::name() => true,
-        ];
+        return (new RelaxPlus)->rules();
     }
 }
