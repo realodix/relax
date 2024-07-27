1b97bb8c359fcff7c12afe2f1bd46cb3f1adf38a
Kei <budi03@gmail.com>
2024-07-28 03:41:32 +0700

    no_superfluous_elseif

--- tests/Fixtures/Ruleset/relaxplus_actual.php
+++ tests/Fixtures/Ruleset/relaxplus_actual.php
@@ -2,6 +2,15 @@
 
 class relaxplus
 {
+    public function control_structure__no_superfluous_elseif()
+    {
+        if (true) {
+            return 1;
+        } elseif (false) {
+            return 2;
+        }
+    }
+
     public function string_notation__explicit_string_variable()
     {
         $name = 'foo';
--- tests/Fixtures/Ruleset/relaxplus_expected.php
+++ tests/Fixtures/Ruleset/relaxplus_expected.php
@@ -2,6 +2,16 @@
 
 class relaxplus
 {
+    public function control_structure__no_superfluous_elseif()
+    {
+        if (true) {
+            return 1;
+        }
+        if (false) {
+            return 2;
+        }
+    }
+
     public function string_notation__explicit_string_variable()
     {
         $name = 'foo';
