0c06a5489d5b31496371910a61cd24d68efad76e
Kei <budi03@gmail.com>
2024-07-29 08:41:30 +0700

    no_leading_namespace_whitespace

--- tests/Fixtures/Ruleset/relax-commonbox2_actual.php
+++ tests/Fixtures/Ruleset/relax-commonbox2_actual.php
@@ -1,5 +1,5 @@
 <?php // blank_line_after_opening_tag
-namespace Realodix\Relax; // blank_lines_before_namespace
+    namespace Realodix\Relax; // blank_lines_before_namespace, no_leading_namespace_whitespace
 // blank_line_after_namespace
 
 // no_closing_tag
